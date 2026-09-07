675ede481b72d1083ee1fc8efb3ba9024002725c
maria45paula <paulaugggvbk@gmail.com>
2026-09-06 21:50:23 -0500

    Registrador de las acciones del robot

---
+++ src/main/scala/logica/MemoriaRobot.scala
@@ -0,0 +1,24 @@
+class MemoriaRobot {
+
+  private var historial: List[String] = List()
+
+  def guardar(accion: String): Unit = {
+    historial = historial :+ accion
+  }
+
+  def obtenerHistorial(): List[String] = {
+    historial
+  }
+
+  def ultimaAccion(): String = {
+
+    if (historial.isEmpty)
+      "Todavía no he hecho nada."
+    else
+      historial.last
+  }
+
+  def cantidadErrores(): Int = {
+    historial.count(accion => accion.contains("ERROR"))
+  }
+}
